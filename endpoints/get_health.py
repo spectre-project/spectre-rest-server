af9e80ba49f9ac93ebfa54deb93e38f28be674b3
x100111010 <167847953+x100111010@users.noreply.github.com>
2024-11-10 23:38:06 +0100

    add `currentBlueScore` and `dbCheck` to `/info/health`
    - added `currentBlueScore` field to `/info/health` endpoint
    - retrieves latest blue score from `get_virtual_chain_blue_score.py` via `getSinkBlueScoreRequest` RPC (which is updating every 5 seconds)
    - holds last known blue score if RPC call fails
    - added `dbCheck`:
    - Valid: Indicates database is up-to-date if the latest block is within
      10 minutes
    - Error - Block Age: Reports "error" with message "Block age older than
      10 minutes" if the last block timestamp exceeds 10 minutes
    - - Error - Database Unavailable: Reports "error" with message
      "Database unavailable" if the database connection fails

--- endpoints/get_health.py
+++ endpoints/get_health.py
@@ -1,15 +1,15 @@
 # encoding: utf-8
 import hashlib
 from datetime import datetime, timedelta
-from typing import List
+from typing import List, Dict
 
-from fastapi import HTTPException
 from pydantic import BaseModel
 from sqlalchemy import select
 
 from dbsession import async_session
 from models.Transaction import Transaction
 from server import app, spectred_client
+from endpoints.get_virtual_chain_blue_score import current_blue_score_data
 
 
 class SpectredResponse(BaseModel):
@@ -22,31 +22,47 @@ class SpectredResponse(BaseModel):
 
 class HealthResponse(BaseModel):
     spectredServers: List[SpectredResponse]
+    currentBlueScore: int = None
+    dbCheck: Dict[str, str]  # report database status
 
 
 @app.get("/info/health", response_model=HealthResponse, tags=["Spectre network info"])
 async def health_state():
     """
-    Checks health by verifying transaction recency and node sync status.
-    Returns each node's status and version.
+    Checks health by verifying node sync status, the recency of the latest block in
+    the database, and returns each node's status, version, and the current blue score.
+
+    If the database check fails, `dbCheck` will show an "error" status and relevant
+    message. If the latest block is older than 10 minutes, it will indicate an outdated
+    status. Otherwise, the status is marked as "valid".
     """
     await spectred_client.initialize_all()
 
     spectreds = []
 
-    async with async_session() as s:
-        last_block_time = (
-            await s.execute(
-                select(Transaction.block_time)
-                .limit(1)
-                .order_by(Transaction.block_time.desc())
-            )
-        ).scalar()
+    # dbCheck status
+    db_check_status = {"status": "valid", "message": "Database is up-to-date"}
 
-    time_diff = datetime.now() - datetime.fromtimestamp(last_block_time / 1000)
+    # check the recency of the latest transaction's block time in the database
+    try:
+        async with async_session() as s:
+            last_block_time = (
+                await s.execute(
+                    select(Transaction.block_time)
+                    .limit(1)
+                    .order_by(Transaction.block_time.desc())
+                )
+            ).scalar()
 
-    if time_diff > timedelta(minutes=10):
-        raise HTTPException(status_code=500, detail="Transactions not up to date")
+        time_diff = datetime.now() - datetime.fromtimestamp(last_block_time / 1000)
+
+        if time_diff > timedelta(minutes=10):
+            db_check_status = {
+                "status": "error",
+                "message": "Block age older than 10 minutes",
+            }
+    except Exception:
+        db_check_status = {"status": "error", "message": "Database unavailable"}
 
     for i, spectred_info in enumerate(spectred_client.spectreds):
         spectreds.append(
@@ -59,4 +75,10 @@ async def health_state():
             }
         )
 
-    return {"spectredServers": spectreds}
+    current_blue_score = current_blue_score_data.get("blue_score")
+
+    return {
+        "spectredServers": spectreds,
+        "currentBlueScore": current_blue_score,
+        "dbCheck": db_check_status,
+    }
